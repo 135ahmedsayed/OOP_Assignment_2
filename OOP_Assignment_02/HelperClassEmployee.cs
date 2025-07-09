58e052ba906c45761a7acf026aaa341644106591
135ahmedsayed <ahmedshkra246@gmail.com>
2025-07-09 20:23:47 +0300

    Q1 Solve

---
+++ OOP_Assignment_02/HelperClassEmployee.cs
@@ -0,0 +1,52 @@
+﻿namespace OOP_Assignment_02;
+
+#region With Q1
+//public class Employees
+//{
+//    private int ID;
+//    private string Name;
+//    private security_privileges security_level;
+//    private double salary;
+//    private DateTime hire_date ;
+//    private Gender Gender;
+
+//    //Constructor
+//    public Employees(int id, string name, security_privileges security_level, double salary, DateTime hire_date, Gender gender)
+//    {
+//        ID = id;
+//        Name = name;
+//        this.security_level = security_level;
+//        this.salary = salary;
+//        this.hire_date = hire_date;
+//        Gender = gender;
+//    }
+//    public override string ToString()
+//    {
+//        return $"ID : {ID} \n" +
+//               $"Name : {Name}\n" +
+//               $"Security Level: {security_level}\n" +
+//               $"Salary: {String.Format(CultureInfo.CurrentCulture, "{0:C}", salary)}\n" +
+//               $"Hire date : {hire_date}\n" +
+//               $"Gender: {Gender}\n" 
+//               ;
+//    }
+//}
+//// Gender
+//public enum Gender
+//{
+//    Male,
+//    Female,
+//    M = 0,
+//    F = 1,
+//};
+////security privileges
+//public enum security_privileges
+//{
+//    guest,
+//    Developer,
+//    secretary,
+//    DBA,
+//};
+
+#endregion
+
--- OOP_Assignment_02/Program.cs
+++ OOP_Assignment_02/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Q1
+            //Employees employees = new Employees(1,"Ahmed Sayed" ,security_privileges.Developer,199109.10010,new DateTime(2001,4,27),Gender.Male);
+            //Console.WriteLine(employees);
+            #endregion
         }
     }
 }
