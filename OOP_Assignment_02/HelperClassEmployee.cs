b98467b2fa48d11cfde759d380aacb21c2f4ac68
135ahmedsayed <ahmedshkra246@gmail.com>
2025-07-09 20:25:18 +0300

    Q2 Solve

--- OOP_Assignment_02/HelperClassEmployee.cs
+++ OOP_Assignment_02/HelperClassEmployee.cs
@@ -50,3 +50,77 @@
 
 #endregion
 
+#region With Q2
+
+//public class Employees
+//{
+//    private int ID;
+//    private string Name;
+//    private security_privileges security_level;
+//    private double salary;
+//    private HireDate Hire_date;
+//    private Gender Gender;
+
+//    // Hire Date
+
+//    public Employees(int id, string name, security_privileges security_level, double salary, HireDate hire_date, Gender gender)
+//    {
+//        ID = id;
+//        Name = name;
+//        this.security_level = security_level;
+//        this.salary = salary;
+//        this.Hire_date = hire_date;
+//        Gender = gender;
+//    }
+//    public override string ToString()
+//    {
+//        return $"ID : {ID} \n" +
+//               $"Name : {Name}\n" +
+//               $"Security Level: {security_level}\n" +
+//               $"Salary: {String.Format(CultureInfo.CurrentCulture, "{0:C}", salary)}\n" +
+//               $"{Hire_date}" +
+//               $"Hire Date : {Hire_date.Day}/{Hire_date.Month}/{Hire_date.Year}\n" +
+//               $"Gender: {Gender}\n"
+//               ;
+//    }
+//}
+//// Gender
+//public enum Gender
+//{
+//    Male,
+//    Female,
+//    M = 0,
+//    F = 1,
+//};
+////security privileges
+//public enum security_privileges
+//{
+//    guest,
+//    Developer,
+//    secretary,
+//    DBA,
+//};
+//// class HireDate
+//public class HireDate
+//{
+//    public int Day;
+//    public int Month;
+//    public int Year;
+
+//    public HireDate(int day, int month, int year)
+//    {
+//        Day = day;
+//        Month = month;
+//        Year = year;
+//    }
+//    public override string ToString()
+//    {
+//        return $"Day   : {Day}\n" +
+//               $"Month : {Month}\n"  + 
+//               $"Year  : {Year}\n"
+//               ;
+//    }
+//}
+
+#endregion
+
--- OOP_Assignment_02/Program.cs
+++ OOP_Assignment_02/Program.cs
@@ -8,6 +8,13 @@
             //Employees employees = new Employees(1,"Ahmed Sayed" ,security_privileges.Developer,199109.10010,new DateTime(2001,4,27),Gender.Male);
             //Console.WriteLine(employees);
             #endregion
+
+            #region Q2
+            //HireDate hireDate = new HireDate(27, 4, 2001);
+            //Employees employees = new Employees(1, "Ahmed Sayed", security_privileges.Developer, 199109.10010, hireDate, Gender.Male);
+            //Console.WriteLine(employees);
+            #endregion
+            Console.ReadKey();
         }
     }
 }
